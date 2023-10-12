e8cc7ae699d3573e27f44cd665588c5732660d50
Wesley Monteiro <wmonteiro873@gmail.com>
2023-10-12 17:12:04 -0300

    voltando para a indicação de modificação

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ console.log("Rodando sistema de gerenciamento de jogos")
 console.log("O sistema está se conectando ao banco de dados")
 // mais um cosole log de teste
 console.log("***modificação***")
-console.log("Enviando para o GitHub")
+
