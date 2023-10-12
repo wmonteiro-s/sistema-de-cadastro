32324ba7f85f25085f21e156d0b15235a01a9ad3
Wesley Monteiro <wmonteiro873@gmail.com>
2023-10-12 16:53:12 -0300

    console.log indicando uma modificação

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 console.log("Rodando sistema de gerenciamento de jogos")
 console.log("O sistema está se conectando ao banco de dados")
+// mais um cosole log de teste
+console.log("***modificação***")
+
